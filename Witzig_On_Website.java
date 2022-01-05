6d1d8a92e109223aa9d8d09dccfac32c216a0ef5
IT-Spezialist-des-Asozialen <89655752+IT-Spezialist-des-Asozialen@users.noreply.github.com>
2022-01-05 12:11:59 +0100

    Update and rename Witzig_On_Website.jar to Witzig_On_Website.java

--- Witzig_On_Website.java
+++ Witzig_On_Website.java
@@ -6,9 +6,11 @@ public class Witzig_On_Website
     static private final long WIT_EMOJI = 577887998310613032;
     static private final long ZIG_EMOJI = 577888095735906305;
     
-    private bool = (event.getEmoji().asCustomEmoji().map(DiscordEntity::getId).orElse(0L) == WITZIG_EMOJI
-     private void reactionAdded(ReactionAddEvent event) {
-        if (event.getEmoji().asCustomEmoji().map(DiscordEntity::getId).orElse(0L) == WITZIG_EMOJI
+    private bool WITZIG = (event.getEmoji().asCustomEmoji().map(DiscordEntity::getId).orElse(0L) == WITZIG_EMOJI
+    private bool WIT_ZIG = (event.getEmoji().asCustomEmoji().map(DiscordEntity::getId).orElse(0L) == WIT_EMOJI && (event.getEmoji().asCustomEmoji().map(DiscordEntity::getId).orElse(0L) == ZIG_EMOJI
+    private bool together = WITZIG || WIT_ZIG
+    private void reactionAdded(ReactionAddEvent event) {
+        if together ==true
                 && event.getChannel().getId() == ZITAT-BOT-SPAM_KANAL) {
             
 
